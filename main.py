116c0693f8eeac15df6febe452e3c9044daf9867
Mikhail Tikhomirov <miguelf0x@yandex.ru>
2023-01-21 23:31:23 +0300

    Moved path, check interval and send interval to config.yaml

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 import asyncio
 import os
 import discord
+import yaml
 from discord.ext import commands
 from dotenv import load_dotenv
 
 CHANNEL_ID = 1061711182085488691
-DELAY = 5
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -31,17 +31,28 @@ def get_new_files(before, after):
 async def on_ready():
     channel = bot.get_channel(CHANNEL_ID)
     while True:
-        before = get_files(PATH)
-        await asyncio.sleep(20)
-        after = get_files(PATH)
+        before = get_files(directory)
+        await asyncio.sleep(check_interval)
+        after = get_files(directory)
 
         for x in list(get_new_files(before, after)):
-            file = f'{PATH}\{x}'
+            file = f'{directory}/{x}'
             await channel.send(file=discord.File(file))
-            await asyncio.sleep(10)
+            await asyncio.sleep(send_interval)
 
 
 if __name__ == "__main__":
+
+    # load envvars
     load_dotenv()
-    PATH = os.environ['PATH']
+
+    # load config
+    with open('config.yaml') as f:
+        try:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+            directory, check_interval, send_interval = data['directory'], data['check_interval'], data['send_interval']
+        except yaml.YAMLError as exception:
+            print(exception)
+
+    # start bot
     bot.run(os.environ['DISCORD_API_KEY'])
