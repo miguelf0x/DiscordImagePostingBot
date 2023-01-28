73c7ab2d1ea67f3ae6cec5008bb70954f3d3ea37
Mikhail Tikhomirov <miguelf0x@yandex.ru>
2023-01-29 00:44:01 +0300

    Added batch generation

--- main.py
+++ main.py
@@ -61,6 +61,49 @@ async def gen(ctx, *, arg):
         await man(ctx)
 
 
+@commands.command(aliases=['b', 'batch', 'mass'])
+async def batch_gen(ctx, *, arg):
+
+    if arg != "":
+
+        trimmed_arg = arg.split(",", 1)
+
+        count = SafeTypes.safe_cast(trimmed_arg[0], "int")
+        tags = SafeTypes.safe_cast(trimmed_arg[1], "str")
+
+        for i in range(0, count):
+
+            if tags != "":
+
+                prompt = dict(PromptTemplate.PROMPT_TEMPLATE)
+                trimmed = tags.split(",", 3)
+
+                steps = SafeTypes.safe_cast(trimmed[0], "int")
+                width = SafeTypes.safe_cast(trimmed[1], "int")
+                height = SafeTypes.safe_cast(trimmed[2], "int")
+
+                if steps > 0 and width > 0 and height > 0:
+                    prompt["steps"] = str(steps)
+                    prompt["width"] = str(width)
+                    prompt["height"] = str(height)
+                    prompt["prompt"] = str(trimmed[3])
+                elif steps > 0:
+                    prompt["prompt"] = str(trimmed[1]) + str(trimmed[2]) + str(trimmed[3])
+                    prompt["steps"] = str(steps)
+                else:
+                    prompt["steps"] = arg
+
+                gen_thread = threading.Thread(target=WebuiRequests.post_generate,
+                                              args=(ctx, prompt, webui_url, post_directory))
+                gen_thread.start()
+
+            else:
+                await man(ctx)
+
+    else:
+        await man(ctx)
+
+
 @commands.command(aliases=['ref', 'refresh'])
 async def refresh_ckpt(ctx):
     await WebuiRequests.post_refresh_ckpt(ctx, webui_url)
@@ -215,6 +258,9 @@ if __name__ == "__main__":
 
     # noinspection PyTypeChecker
     bot.add_command(gen)
+
+    # noinspection PyTypeChecker
+    bot.add_command(batch_gen)
 
     # noinspection PyTypeChecker
     bot.add_command(refresh_ckpt)
