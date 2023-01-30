31b357cb407769670fcb375b0a95de0821ef9676
Mikhail Tikhomirov <miguelf0x@yandex.ru>
2023-01-31 00:29:29 +0300

    Changed negative prompt to TRIM_NEGATIVE After some testing I feel like trimmed negative prompt shows better result at colors and details

--- PromptTemplate.py
+++ PromptTemplate.py
@@ -1,18 +1,29 @@
-NEGATIVE_PROMPT_4X = "((((ugly)))), "
-NEGATIVE_PROMPT_3X = "(((duplicate))), (((mutation))), (((deformed))), (((bad proportions))), (((disfigured))), " \
-                     "(((extra arms))), (((extra legs))), (((long neck))), (((worst quality))), "
-NEGATIVE_PROMPT_2X = "((morbid)), ((mutilated)), ((poorly drawn hands)), ((bad anatomy)), ((extra limbs)), " \
-                     "((out of frame)), ((missing arms)), ((missing legs)), "
-NEGATIVE_PROMPT_1X = "(malformed limbs), (fused fingers), (too many fingers), (watermark), "
-NEGATIVE_PROMPT_0X = "extra fingers, mutated hands, blurry, cloned face, gross proportions, jpeg artifacts, " \
-                     "signature, username"
+LONG_NEGATIVE_PROMPT_4X = "((((ugly)))), "
+LONG_NEGATIVE_PROMPT_3X = "(((duplicate))), (((mutation))), (((deformed))), (((bad proportions))), (((disfigured))), " \
+                          "(((extra arms))), (((extra legs))), (((long neck))), (((worst quality))), "
+LONG_NEGATIVE_PROMPT_2X = "((morbid)), ((mutilated)), ((poorly drawn hands)), ((bad anatomy)), ((extra limbs)), " \
+                          "((out of frame)), ((missing arms)), ((missing legs)), "
+LONG_NEGATIVE_PROMPT_1X = "(malformed limbs), (fused fingers), (too many fingers), (watermark), "
+LONG_NEGATIVE_PROMPT_0X = "extra fingers, mutated hands, blurry, cloned face, gross proportions, jpeg artifacts, " \
+                          "signature, username"
 
-NEGATIVE_PROMPT_CONCAT = NEGATIVE_PROMPT_4X + NEGATIVE_PROMPT_3X + NEGATIVE_PROMPT_2X + NEGATIVE_PROMPT_1X + \
-                         NEGATIVE_PROMPT_0X
+LONG_NEGATIVE_PROMPT_CONCAT = LONG_NEGATIVE_PROMPT_4X + LONG_NEGATIVE_PROMPT_3X + LONG_NEGATIVE_PROMPT_2X + \
+                              LONG_NEGATIVE_PROMPT_1X + LONG_NEGATIVE_PROMPT_0X
+
+TRIM_NEGATIVE_PROMPT_4X = "((((ugly)))), ((((gross)))), "
+TRIM_NEGATIVE_PROMPT_3X = "(((mutation))), (((deformed))), (((bad proportions))), (((disfigured))), " \
+                          "(((extra arms))), (((extra legs))),"
+TRIM_NEGATIVE_PROMPT_2X = "((morbid)), ((mutilated)), ((poorly drawn hands)), ((bad anatomy)), ((extra limbs)), " \
+                          "((out of frame)), ((missing arms)), ((missing legs)),"
+TRIM_NEGATIVE_PROMPT_1X = "(malformed), (fused fingers), (too many fingers), (watermark),"
+TRIM_NEGATIVE_PROMPT_0X = "extra fingers, blurry, cloned face, artifacts, signature, username"
+
+TRIM_NEGATIVE_PROMPT_CONCAT = TRIM_NEGATIVE_PROMPT_4X + TRIM_NEGATIVE_PROMPT_3X + TRIM_NEGATIVE_PROMPT_2X + \
+                              TRIM_NEGATIVE_PROMPT_1X + TRIM_NEGATIVE_PROMPT_0X
 
 PROMPT_TEMPLATE = {
     "prompt": "1girl, standing, blue_hair",
-    "negative_prompt": NEGATIVE_PROMPT_CONCAT,
+    "negative_prompt": TRIM_NEGATIVE_PROMPT_CONCAT,
     "steps": 60,
     "width": 512,
     "height": 512,
